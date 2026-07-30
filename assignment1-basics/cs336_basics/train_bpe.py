ccd5b4abd9e0860e24d4edcce618235e25ff699a
Tenshi0x0 <2478906339@qq.com>
2026-07-30 07:31:14 -0400

    A1: train_bpe v0, fail3

--- assignment1-basics/cs336_basics/train_bpe.py
+++ assignment1-basics/cs336_basics/train_bpe.py
@@ -1,5 +1,6 @@
 import os
 from cs336_basics.utils import pretokenization
+import regex as re
 
 
 def train(
@@ -29,4 +30,65 @@ def train(
                 representing that <token1> was merged with <token2>.
                 Merges are ordered by order of creation.
     """
-    
+    # init
+    vocab: dict[int, bytes] = {}
+    vocab_id = 0
+    merges: list[tuple[bytes, bytes]] = []
+    freq: dict[tuple[bytes, ...], int] = {}
+    # can vocab_size be too small to restore these vocabs??
+    for e in special_tokens:
+        vocab[vocab_id] = e.encode("utf-8")
+        vocab_id += 1
+    for i in range(256):
+        vocab[vocab_id] = bytes([i])
+        vocab_id += 1
+
+    # is eot always in special_tokens?
+    pattern = "|".join(map(re.escape, special_tokens))
+    PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+
+    with open(input_path, "rb") as f:
+        num_processes = 4
+        boundaries = pretokenization.find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
+
+        for start, end in zip(boundaries[:-1], boundaries[1:]):
+            f.seek(start)
+            chunk = f.read(end - start).decode("utf-8", errors="ignore")
+
+            strs = chunk.split(pattern)
+            for cur_string in strs:
+                words = re.findall(PAT, cur_string)
+                for word in words:
+                    b_word = word.encode("utf-8")
+                    tup: tuple[bytes, ...] = tuple(bytes([b]) for b in b_word)
+                    freq[tup] = freq.get(tup, 0) + 1
+
+    # merging:
+    while True:
+        if vocab_id >= vocab_size:
+            break
+        freq_bp = {}
+        for b_tup, cnt in freq.items():
+            for i in range(0, len(b_tup) - 1):
+                byte_pair = b_tup[i : i + 2]
+                freq_bp[byte_pair] = freq_bp.get(byte_pair, 0) + cnt
+        if not freq_bp:  # no more word to merge
+            break
+        best_pair = max(freq_bp, key=lambda k: (freq_bp[k], k))
+        merges.append(best_pair)
+        vocab[vocab_id] = b"".join(best_pair)
+        vocab_id += 1
+        new_freq = {}
+        for b_tup, cnt in freq.items():
+            new_b_list = []
+            i = 0
+            while i < len(b_tup):
+                if i + 1 < len(b_tup) and b_tup[i : i + 2] == best_pair:
+                    new_b_list.append(b"".join(b_tup[i : i + 2]))
+                    i += 2
+                else:
+                    new_b_list.append(b_tup[i : i + 1])
+                    i += 1
+            new_freq[tuple(new_b_list)] = new_freq.get(tuple(new_b_list), 0) + cnt
+
+    return (vocab, merges)
